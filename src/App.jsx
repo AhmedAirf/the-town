030352a8e5d0904c7e7d3cd2088cb3e5a0f2f91d
AhmedAirf <ahmed.airf.x@gmail.com>
2025-02-21 19:05:27 +0200

    fifth

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Introduction from "./components/Home/Introduction";
-
 import Home from "./components/Home/Home";
 import OurWork from "./components/Home/OurWork";
+import Introduction from "./components/Home/Introduction";
 
 function App() {
   return (
     <Router>
       <Header />
+
       <Home />
 
       <Routes>
+        <Route path="/" element={<Home />} />
+
         <Route path="/introduction" element={<Introduction />} />
         <Route path="/our-work" element={<OurWork />} />
         <Route path="/contact-us" element={<Footer />} />
       </Routes>
+
+      <Footer />
     </Router>
   );
 }
